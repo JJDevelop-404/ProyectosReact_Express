a95c75b7542f6ead92835a73ea4b73018a98d070
JJMarin-08345 <marinjuanjose2005@gmail.com>
2024-03-20 23:25:27 -0500

    Se agrega filtrado por nombre y categorias

--- DinnerSys_Back/src/pages/Mesero/Pedido.jsx
+++ DinnerSys_Back/src/pages/Mesero/Pedido.jsx
@@ -20,40 +20,57 @@ export default function Pedido() {
     setDesplegarDescripcion(desplegarDescripcion === productoId ? false : productoId);
   };
 
-  const togglePrecio = (productoId) => {
+  const onHandleClickInPrecio = (productoId) => {
     lstIdProductos.push(productoId);
     console.log(lstIdProductos);
   }
-  
-  const [filtro, setFiltro] = useState(null);
 
-  const onHandleFiltrar = (value) => {
-    if (value === "categoria") {
-      setFiltro('Postres');
-    }else{
+  // Variables para la busqueda de productos
+  const [inputBuscar, setInputBuscar] = useState('');
+  const [filtro, setFiltro] = useState(['nombre', '', inputBuscar]);
+
+
+  const onHandleFiltrar = (valueSelect) => {
+    if (valueSelect === "nombre") {
+      setFiltro(['nombre', '', inputBuscar]);
+    } else if (valueSelect === "categoria") {
+      setFiltro(['categoria', 'Comidas', inputBuscar]);
+    }else if (valueSelect === 'precio_mayor'){
+      setFiltro(['precio_mayor', '', inputBuscar])
+    
+    } else {
       setFiltro(null);
     }
   }
-  
+
   const productosFiltrados = productos.filter((producto) => {
-    return filtro === null ?  producto :
-    producto.Categoria.includes(filtro) 
+    // console.log(filtro);
+    return filtro && filtro[0] === 'nombre' ? producto.Nombre.toLowerCase().includes(filtro[2].toLowerCase()) :
+      filtro && filtro[0] === 'categoria' ? producto.Categoria === filtro[1] && producto.Nombre.toLowerCase().includes(filtro[2].toLowerCase()) :
+        producto;
+
   })
 
   return (
     <div className="container pedido">
-      
+
       <h2 className='title-pedido'>Productos</h2>
 
       <div className="container-filtros">
-        <input className='input-buscar' type="text" placeholder="Buscar Producto" />
-        <select onChange={(ev)=>onHandleFiltrar(ev.target.value)} className='select-filtros'>
-          <option value="0">Filtrar Por</option>
+        <input className='input-buscar' type="text" placeholder="Buscar Producto" onChange={(ev) => { setInputBuscar(ev.target.value), setFiltro([filtro[0], filtro[1], ev.target.value]) }} />
+        <select onChange={(ev) => onHandleFiltrar(ev.target.value)} className='select-filtros'>
           <option value="nombre">Nombre</option>
           <option value="categoria">Categoria</option>
-          <option value="precio_mayor">Precio Mayor a Menor</option>
-          <option value="precio_menor">Precio Menor a Mayor</option>
+          {/* <option value="precio_mayor">Precio Mayor a Menor</option>
+          <option value="precio_menor">Precio Menor a Mayor</option> */}
         </select>
+        {filtro && filtro[0] === 'categoria' &&
+          <select className='select-filtros' onChange={(ev) => { setFiltro([filtro[0], ev.target.value, inputBuscar]) }} >
+            <option value="Comidas"> Comidas </option>
+            <option value="Bebidas"> Bebidas </option>
+            <option value="Postres"> Postres </option>
+          </select>
+        }
       </div>
 
 
@@ -64,7 +81,7 @@ export default function Pedido() {
               {producto.Nombre} <i className={desplegarDescripcion === producto.ProductoId ? 'fa-solid fa-caret-up' : 'fa-solid fa-caret-down'} ></i>
               {desplegarDescripcion === producto.ProductoId ? <p className='pr-descripcion' onClick={() => toggleDescripcion(producto.ProductoId)}> {producto.Descripcion} </p> : null}
             </h3>
-            <h3 className='pr-precio' onClick={() => togglePrecio(producto.ProductoId)}>${producto.Precio}</h3>
+            <h3 className='pr-precio' onClick={() => onHandleClickInPrecio(producto.ProductoId)}>${producto.Precio}</h3>
           </div>
         ))}
       </div>
